caeebfc7ee69009eb8472cd80278deea4611aaea
leo <18816793306@163.com>
2018-12-22 14:13:26 +0800

    update

--- src/main/java/com/mmall/task/Task.java
+++ src/main/java/com/mmall/task/Task.java
@@ -1,11 +1,11 @@
 package com.mmall.task;
 
 import com.mmall.config.RedisOperator;
-import lombok.extern.apachecommons.CommonsLog;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
-@CommonsLog
+@Component
 @Slf4j
 public class Task {
 
